46f6237f6f3a54b95b1641c73fac9e060f1be5ce
Daniel Burghieri Dezuani <daniel.dezuani@hotmail.com>
2018-08-12 15:14:01 -0300

    CameraBasica

--- Ninj'em Up/Assets/Scripts/MainCamera.cs
+++ Ninj'em Up/Assets/Scripts/MainCamera.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static string estadoCamera;
+
+    public GameObject playerAtual;
+    //public GameObject limitEsq;
+    public GameObject camLimitDir;
+    public GameObject limitDir;
+    public Transform mainCamera;
+
+    private void Awake()
+    {
+        estadoCamera = "followP1";
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+        CameraLimits();
+        CameraFollow();
+    }
+
+    public void CameraFollow()
+    {
+        if (estadoCamera == "none")
+        {
+            mainCamera.position = Vector3.Lerp(mainCamera.position, new Vector3(mainCamera.transform.position.x,
+                mainCamera.position.y, mainCamera.position.z), 1f);
+        }
+
+        else if (estadoCamera == "followP1")
+        {
+            //faz a camera seguir o personagem da esquerda
+            mainCamera.position = Vector3.Lerp(mainCamera.position, new Vector3(playerAtual.transform.position.x + 5.5f,
+                mainCamera.position.y, mainCamera.position.z), 1f);
+        }
+
+        else if (estadoCamera == "followP2")
+        {
+            //faz a camera seguir o personagem da direita
+            mainCamera.position = Vector3.Lerp(mainCamera.position, new Vector3(playerAtual.transform.position.x - 5.5f,
+                mainCamera.position.y, mainCamera.position.z), 1f);
+        }
+    }
+
+    public void CameraLimits()
+    {
+        if (camLimitDir.transform.position.x >= limitDir.transform.position.x)
+        {
+            estadoCamera = "none";
+        }
+    }
 }
